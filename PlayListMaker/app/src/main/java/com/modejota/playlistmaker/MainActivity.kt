1d8ca65cdbbeda1c4660921472cbecd6cd61baf6
José Alberto Gómez García <modej@correo.ugr.es>
2022-04-19 20:15:16 +0200

    Delete playlist File from UI

--- PlayListMaker/app/src/main/java/com/modejota/playlistmaker/MainActivity.kt
+++ PlayListMaker/app/src/main/java/com/modejota/playlistmaker/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.modejota.playlistmaker
 
 import android.Manifest
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.modejota.playlistmaker.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PlaylistAdapter.OnItemClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private var actionMenu: Menu? = null
+    private val selectedPaths = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +39,7 @@ class MainActivity : AppCompatActivity() {
             binding.swipeRefreshLayout.isRefreshing = false
         }
         SharedData.clearAll()
+
     }
 
     override fun onStart() {
@@ -61,18 +68,72 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        actionMenu = menu
+        menuInflater.inflate(R.menu.action_bar_delete_playlist, menu)
+        showActionMenu(false)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun showActionMenu(show: Boolean) {
+        actionMenu?.let {
+            it.findItem(R.id.selectedPlaylistDelete).isVisible = show
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.selectedPlaylistDelete -> {
+                confirmDelete()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun confirmDelete() {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle(getString(R.string.delete_playlist_title))
+        alertDialog.setMessage(getString(R.string.delete_playlist_message))
+        alertDialog.setPositiveButton(getString(R.string.affirmative)) { _, _ ->
+            selectedPaths.forEach {
+                Utilities.deletePlaylistFileFromStorage(it)
+            }
+            initRecyclerView()
+            Toast.makeText(this, getString(R.string.confirm_playlist_deleted), Toast.LENGTH_SHORT).show()
+        }
+        alertDialog.setNegativeButton(getString(R.string.negative)) { _, _ -> }
+        alertDialog.show()
+    }
+
     private fun initRecyclerView() {
         binding.rvPlaylistList.layoutManager = LinearLayoutManager(this)
         val playlistsList = Utilities.getPlaylistsData()
-        binding.rvPlaylistList.adapter = PlaylistAdapter(playlistsList)
-        (binding.rvPlaylistList.adapter as PlaylistAdapter).onItemClick={
-            val adapter = binding.rvPlaylistList.adapter as PlaylistAdapter
-            val item = playlistsList.indexOfFirst { playlist -> playlist == it }
-            SharedData.setPlaylistPath(adapter.getItem(item).path)
-            SharedData.setAllSongs(Utilities.getMusicFromInternalStorage(this))
-            val intent = Intent(this, PlaylistManagementActivity::class.java)
-            startActivity(intent)
+        val isClickedList = mutableListOf<Boolean>()
+        isClickedList.addAll(Array(playlistsList.size) { false })
+        binding.rvPlaylistList.adapter = PlaylistAdapter(playlistsList, isClickedList, this)
+        selectedPaths.clear()
+        showActionMenu(false)
+    }
+
+    override fun onItemClick(position: Int) {
+        val adapter = binding.rvPlaylistList.adapter as PlaylistAdapter
+        SharedData.setPlaylistPath(adapter.getItem(position).path)
+        SharedData.setAllSongs(Utilities.getMusicFromInternalStorage(this))
+        val intent = Intent(this, PlaylistManagementActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onItemLongClick(position: Int) {
+        val adapter = binding.rvPlaylistList.adapter as PlaylistAdapter
+        val item = adapter.getItem(position).path
+        if (selectedPaths.contains(item)) {
+            selectedPaths.remove(item)
+        } else {
+            selectedPaths.add(item)
         }
+        adapter.changeVisibility(position)
+        showActionMenu(selectedPaths.isNotEmpty())
+
     }
 
     private fun askForPermission() {
--- PlayListMaker/app/src/main/java/com/modejota/playlistmaker/PlaylistAdapter.kt
+++ PlayListMaker/app/src/main/java/com/modejota/playlistmaker/PlaylistAdapter.kt
@@ -2,12 +2,15 @@ package com.modejota.playlistmaker
 
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.modejota.playlistmaker.databinding.ItemPlaylistBinding
 
-class PlaylistAdapter(private val playlistList: List<Playlist>) : RecyclerView.Adapter<PlaylistAdapter.PlaylistViewHolder>() {
-
-    var onItemClick: ((Playlist) -> Unit)? = null
+class PlaylistAdapter(
+    private val playlistList: List<Playlist>,
+    private val isClickedList: MutableList<Boolean>,
+    private val listener: OnItemClickListener,
+    ) : RecyclerView.Adapter<PlaylistAdapter.PlaylistViewHolder>() {
 
     override fun onCreateViewHolder(parent: android.view.ViewGroup, viewType: Int): PlaylistViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -16,20 +19,41 @@ class PlaylistAdapter(private val playlistList: List<Playlist>) : RecyclerView.A
 
     override fun onBindViewHolder(holder: PlaylistViewHolder, position: Int) {
         holder.render(playlistList[position])
+        holder.changeVisibility(isClickedList[position])
     }
 
     override fun getItemCount(): Int = playlistList.size
 
+    fun changeVisibility(position: Int) {
+        isClickedList[position] = !isClickedList[position]
+        notifyItemChanged(position)
+    }
+
     fun getItem(position: Int): Playlist = playlistList[position]
 
-    inner class PlaylistViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    inner class PlaylistViewHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnClickListener, View.OnLongClickListener {
 
         private val binding = ItemPlaylistBinding.bind(view)
 
         init {
-            itemView.setOnClickListener {
-                onItemClick?.invoke(playlistList[adapterPosition])
+            view.setOnClickListener(this)
+            view.setOnLongClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
             }
+        }
+
+        override fun onLongClick(p0: View?): Boolean {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemLongClick(position)
+            }
+            return true
         }
 
         fun render(playlist: Playlist) {
@@ -38,6 +62,19 @@ class PlaylistAdapter(private val playlistList: List<Playlist>) : RecyclerView.A
                 itemView.context.resources.getQuantityString(R.plurals.playlist_size, playlist.size, playlist.size)
         }
 
+        fun changeVisibility(selected: Boolean) {
+            if (selected) {
+                binding.selectedTickPlaylist.visibility = View.VISIBLE
+            } else {
+                binding.selectedTickPlaylist.visibility = View.INVISIBLE
+            }
+        }
+
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+        fun onItemLongClick(position: Int)
     }
 
 }
