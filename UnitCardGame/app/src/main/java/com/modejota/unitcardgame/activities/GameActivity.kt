ab9d320af85e20929ce7673ac26519d2d1736442
José Alberto Gómez García <modej@correo.ugr.es>
2022-05-12 18:47:13 +0200

    Send something locally

--- UnitCardGame/app/src/main/java/com/modejota/unitcardgame/activities/GameActivity.kt
+++ UnitCardGame/app/src/main/java/com/modejota/unitcardgame/activities/GameActivity.kt
@@ -6,11 +6,13 @@ import android.os.StrictMode.ThreadPolicy
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.modejota.unitcardgame.clientstuff.Jugador
 import com.modejota.unitcardgame.databinding.ActivityGameBinding
 import com.modejota.unitcardgame.model.Card
 import com.modejota.unitcardgame.model.CardType
 import com.modejota.unitcardgame.otherstuff.CardAdapter
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.lang.Thread.sleep
 import java.net.ServerSocket
 import java.net.Socket
 
@@ -18,7 +20,6 @@ import java.net.Socket
 class GameActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGameBinding
-    private var jugador: Jugador? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,30 +34,21 @@ class GameActivity : AppCompatActivity() {
         Toast.makeText(this, "IP RECIBIDA: $ip", Toast.LENGTH_LONG).show()
         val amIServer = intent.getBooleanExtra("AM_I_SERVER", false)
         if (amIServer) {
-            //jugador = Jugador(ip!!, this)
-            //jugador!!.crearPartida(2)
-
-            /*
-            // Dandose cuenta de que IP != 0.0.0.0, esto funciona rapidismo, probar con lo otro.
-            val server = ServerSocket(9029)
-            println("Server running on port ${server.localPort}")
-            val client = server.accept()
-            println("Client connected from ${client.inetAddress}")
-            */
+            val serverSocket = ServerSocket(9029)
+            val socket = serverSocket.accept()
+            sleep(2000)
+            val din = DataInputStream(socket.getInputStream())
+            val message = din.readUTF()
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 
         } else {
             // Aqui habría que poner un cartelito diciendo "Espere a que se conecte el servidor"
-            //jugador = Jugador(ip!!, this)
-            //jugador!!.unirseAPartida()
-
-            /*
-            // Dandose cuenta de que IP != 0.0.0.0, esto funciona rapidismo, probar con lo otro.
-            val socket = Socket(ip!!, 9029)
-            println("Client connected")
-            */
-
+            val socket = Socket(ip, 9029)
+            val dout = DataOutputStream(socket.getOutputStream())
+            dout.writeUTF("HOLA ME HE ENVIADO ALGO")
+            dout.flush()
+            Toast.makeText(this, "CLIENTE CONECTADO", Toast.LENGTH_LONG).show()
         }
-        //Toast.makeText(this, "Conectado: ${jugador!!.getSocket()?.isConnected.toString()}", Toast.LENGTH_LONG).show()
 
         binding.rvMyCards.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
